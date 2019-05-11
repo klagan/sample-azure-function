a48a7b9342ec80627bf991e24bd7719a317aa9c3
Kam Lagan <kam.lagan@aprexo.com>
2019-05-11 21:36:44 +0000

    added message receiver handler for testing

--- src/Samples.FunctionApp/AutomaticFunction.cs
+++ src/Samples.FunctionApp/AutomaticFunction.cs
@@ -7,6 +7,10 @@ namespace Samples.FunctionApp
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Logging;
     using Newtonsoft.Json;
+    using Microsoft.Azure.ServiceBus.Core;
+    using System;
+    using System.Threading;
+    using Microsoft.Azure.ServiceBus;
 
     public static class Function1
     {
@@ -15,7 +19,7 @@ namespace Samples.FunctionApp
         [return: ServiceBus("my-out-topic", Connection = "ConnectionStrings:myServiceBusWrite")]
         public static async Task<string> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger log, MessageReceiver messageReceiver)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -24,6 +28,24 @@ namespace Samples.FunctionApp
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            messageReceiver.RegisterMessageHandler(async (message, token) =>
+                    {
+                        Console.WriteLine($"Received message: Id:{message.MessageId} Body:{message.Body}");
+
+                        // Complete the message so that it is not received again.
+                        // This can be done only if the queueClient is opened in ReceiveMode.PeekLock mode.
+                        //await queueClient.CompleteAsync(message.LockToken);
+                        await Task.CompletedTask;
+                    },
+                    new MessageHandlerOptions(args =>
+                    {
+                        Console.WriteLine($"Message handler encountered an exception {args.Exception}.");
+                        return Task.CompletedTask;
+                    })
+                        { AutoComplete = false, MaxConcurrentCalls = 32 }
+                    );
+
 
             return name;
         }
